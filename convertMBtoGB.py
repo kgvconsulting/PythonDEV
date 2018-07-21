b26310d52b52d927a8210201f0249b89d6306cf2
Krasimir Vatchinsky <kgvconsulting@users.noreply.github.com>
2018-07-21 12:21:53 -0400

    Create convertMBtoGB.py
    This program help converting megabytes to gigabytes

---
+++ convertMBtoGB.py
@@ -0,0 +1,10 @@
+# Created by Krasimir Vatchinsky - KGV Consulting Corp - info@kgvconsultingcorp.com
+# This program help converting megabytes to gigabytes
+
+# convert megabytes to gigabytes
+
+mb = input("entera number of megabytes: ")
+mb = float(mb)
+gb = mb / 1024
+
+print(mb, "megabytes is = to",gb, "gigabytes")
