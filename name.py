46f35ccff57568b72663dd2d940599776b8e12a6
Krasimir Vatchinsky <kgvconsulting@users.noreply.github.com>
2018-07-21 13:04:53 -0400

    Create name.py
    cool fun program

---
+++ name.py
@@ -0,0 +1,15 @@
+# Created by Krasimir Vatchinsky - KGV Consulting Corp - info@kgvconsultingcorp.com
+# display sentence by entered user input
+
+
+name = input("Enter your first name: ")
+
+number = input("Enter your favorite positive integer:  ")
+
+number = int(number)
+
+color = input("Enter you favorite color: ")
+
+newNum = number * 3
+
+print("Hi,", name, "your favorite color is", color, "and your new number is:", newNum)
